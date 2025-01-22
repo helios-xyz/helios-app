c40e842dd3d0ad80f1de6f2e2ac5c1b5aac79328
Luke Nittmann <luke.nittmann@gmail.com>
2025-01-21 18:37:59 -0800

    fix: update next.config.js to use ESM imports

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,4 @@
-const { config } = require('@repo/next-config');
+import { config } from '@repo/next-config';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -9,8 +9,12 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'assets.basehub.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
